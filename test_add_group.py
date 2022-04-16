e113a4e75838ad68082048d8d7aa8f0081ca85e1
Aleksey Zhdanov <ZhdanovAV@bcs.ru>
2022-04-16 09:41:07 +0300

    добавил нужные библиотеки и драйвер. Тест заработал

--- test_add_group.py
+++ test_add_group.py
@@ -7,6 +7,7 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re
 
+
 class GroupCreationTests(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
@@ -14,7 +15,7 @@ class GroupCreationTests(unittest.TestCase):
         self.base_url = "https://www.google.com/"
         self.verificationErrors = []
         self.accept_next_alert = True
-    
+
     def test_group_creation_tests(self):
         driver = self.driver
         driver.get("http://localhost/addressbook/group.php")
@@ -41,17 +42,21 @@ class GroupCreationTests(unittest.TestCase):
         driver.find_element_by_name("submit").click()
         driver.find_element_by_link_text("group page").click()
         driver.find_element_by_link_text("Logout").click()
-    
+
     def is_element_present(self, how, what):
-        try: self.driver.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
+        try:
+            self.driver.find_element(by=how, value=what)
+        except NoSuchElementException as e:
+            return False
         return True
-    
+
     def is_alert_present(self):
-        try: self.driver.switch_to_alert()
-        except NoAlertPresentException as e: return False
+        try:
+            self.driver.switch_to_alert()
+        except NoAlertPresentException as e:
+            return False
         return True
-    
+
     def close_alert_and_get_its_text(self):
         try:
             alert = self.driver.switch_to_alert()
@@ -61,11 +66,13 @@ class GroupCreationTests(unittest.TestCase):
             else:
                 alert.dismiss()
             return alert_text
-        finally: self.accept_next_alert = True
-    
+        finally:
+            self.accept_next_alert = True
+
     def tearDown(self):
         self.driver.quit()
         self.assertEqual([], self.verificationErrors)
+
 
 if __name__ == "__main__":
     unittest.main()
