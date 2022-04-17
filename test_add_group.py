57318f0143ec14eea95b5b8a3d24609c647bb54b
Aleksey Zhdanov <ZhdanovAV@bcs.ru>
2022-04-17 08:21:52 +0300

    выделил класс Application

--- test_add_group.py
+++ test_add_group.py
@@ -2,79 +2,32 @@
 
 import unittest
 
-from selenium import webdriver
-
+from application import Application
 from group import Group
 
 
-# Пока будем использовать unittest - входит в стандартную библиотеку Python
-# В unittest требуется чтобы тестовый класс являлся наследником специального класса - unittest.TestCase
 class GroupCreationTests(unittest.TestCase):
+    # Данный метод будет инициализировать фикстуру:
     def setUp(self):
-        print("\n\n********** setUp **********")
-        self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
+        print("\n\n******************* setUp ********************")
+        self.app = Application()
 
+    # Метод завершения:
     def tearDown(self):
-        print("********** tearDown **********\n")
-        self.wd.quit()
-
-    def open_home_page(self):
-        print("  вспомогательная функция open_home_page()")
-        wd = self.wd
-        wd.get("http://localhost/addressbook/")
-
-    def login(self, username, password):
-        print("  вспомогательная функция login()")
-        print("    username: " + username)
-        print("    password: " + password)
-        wd = self.wd
-        self.open_home_page()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-
-    def open_groups_page(self):
-        print("  вспомогательная функция open_groups_page()")
-        wd = self.wd
-        wd.find_element_by_link_text("groups").click()
-
-    def create_group(self, group):
-        print("  вспомогательная функция create_group()")
-        print("    group: " + str(group))
-        wd = self.wd
-        self.open_groups_page()
-        # Init group creation:
-        wd.find_element_by_name("new").click()
-        # Fill group form:
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # Submit group creation:
-        wd.find_element_by_name("submit").click()
-        self.return_to_groups_page()
-
-    def return_to_groups_page(self):
-        print("  вспомогательная функция return_to_groups_page()")
-        wd = self.wd
-        wd.find_element_by_link_text("group page").click()
-
-    def logout(self):
-        print("  вспомогательная функция logout()")
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
+        print("\n\n****************** tearDown ******************")
+        self.app.destroy()
 
     def test_add_group(self):
-        print("Test add group")
-        self.login(username="admin", password="secret")
-        self.create_group(Group(name="Name 1", header="Header 1", footer="Footer 1"))
-        self.logout()
+        print("\n\n*************** Test add group ***************")
+        self.app.login(username="admin", password="secret")
+        self.app.create_group(Group(name="Name 1", header="Header 1", footer="Footer 1"))
+        self.app.logout()
 
     def test_add_empty_group(self):
-        print("Test add empty group")
-        self.login(username="admin", password="secret")
-        self.create_group(Group(name="", header="", footer=""))
-        self.logout()
+        print("\n\n************ Test add empty group ************")
+        self.app.login(username="admin", password="secret")
+        self.app.create_group(Group(name="", header="", footer=""))
+        self.app.logout()
 
 
 # Подсказка для Python как именно этот скрипт должен запускаться, в том случае если мы
