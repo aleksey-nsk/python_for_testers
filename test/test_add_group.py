612e8e48ea6f107dfed4ed1761050b8ccb6c9aa1
Aleksey Zhdanov <ZhdanovAV@bcs.ru>
2022-04-17 10:48:57 +0300

    refactoring: раскидал файлы по разным пакетам для удобства

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -2,8 +2,8 @@
 
 import pytest
 
-from application import Application
-from group import Group
+from fixture.application import Application
+from model.group import Group
 
 
 @pytest.fixture  # чтобы pytest догадался, что это не просто функция, а функция создающая фикстуру
