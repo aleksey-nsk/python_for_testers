8ef71bf9afb92215297642e84cede8e38dc3ff29
Aleksey Zhdanov <ZhdanovAV@bcs.ru>
2022-06-07 13:56:31 +0300

    Сравнение списков групп - update

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -8,14 +8,24 @@ from model.group import Group
 def test_add_group(app):
     print("\n\n*************** Test add group ***************")
     old_groups = app.group.get_group_list()
-    app.group.create(Group(name="Name 1", header="Header 1", footer="Footer 1"))
+    group = Group(name="Name 1", header="Header 1", footer="Footer 1")
+    app.group.create(group)
+
     new_groups = app.group.get_group_list()
     assert len(new_groups) == len(old_groups) + 1
+
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 def test_add_empty_group(app):
     print("\n\n************ Test add empty group ************")
     old_groups = app.group.get_group_list()
-    app.group.create(Group(name="", header="", footer=""))
+    group = Group(name="", header="", footer="")
+    app.group.create(group)
+
     new_groups = app.group.get_group_list()
     assert len(new_groups) == len(old_groups) + 1
+
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -6,9 +6,14 @@ from model.group import Group
 def test_modify_group_name(app):
     print("\n\n************ Test modify group name **********")
     old_groups = app.group.get_group_list()
-    app.group.modify_first(Group(name="New group name"))
+    group = Group(id=old_groups[0].id, name="New group name")
+    app.group.modify_first(group)
+
     new_groups = app.group.get_group_list()
     assert len(new_groups) == len(old_groups)
+
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 def test_modify_group_header(app):
