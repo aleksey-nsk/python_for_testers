fef46d7157559416d99720099c735b918dcb2b03
Aleksey Zhdanov <ZhdanovAV@bcs.ru>
2022-06-07 15:08:33 +0300

    Хеширование

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -10,10 +10,9 @@ def test_add_group(app):
     old_groups = app.group.get_group_list()
     group = Group(name="Name 1", header="Header 1", footer="Footer 1")
     app.group.create(group)
+    assert app.group.count() == len(old_groups) + 1
 
     new_groups = app.group.get_group_list()
-    assert len(new_groups) == len(old_groups) + 1
-
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
